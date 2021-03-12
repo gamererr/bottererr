b6f6de994b974d22eaf45bc790ebc32cd56f845f
gamererr <starkid2u@outlook.com>
2021-03-12 12:48:42 -0500

    pog is alive :crab:

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+import json
+import random
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='g!', intents=intents)
@@ -20,6 +22,7 @@ async def ping(ctx):
 async def on_member_join(member):
 	welcome = discord.utils.get(member.guild.channels, id=766848918499360809)
 
+	await member.send(f"welcome to **{members.guild.name}**")
 	await welcome.send(f"{member.mention} has joined the server\n\nwe now have {len(member.guild.members)} members")
 
 @client.event
@@ -28,13 +31,19 @@ async def on_member_remove(member):
 
         await welcome.send(f"{member.mention} has left the server\n\nwe now have {len(member.guild.members)} members")
 
-pog = ["pog","poj","poog","pooog","pooj","poooj", "po𝑔"]
-
 @client.event
 async def on_message(message):
+
+	if message.author == client.user:
+		return
+
+	with open("pog.json", "rt") as pograw:
+                pog = json.loads(pograw.read())
+
 	for x in pog:
 		if x in message.content:
 			try:
+				await message.author.send("https://discord.gg/ygPF4XH you were kicked for saying p*g")
 				await message.guild.kick(message.author, reason="said p*g")
 				print(f"{message.author} said p*g")
 			except discord.errors.Forbidden:
