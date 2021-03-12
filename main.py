2ade41fdb3ee8ef0d8063503e1694e9222effc29
gamererr <starkid2u@outlook.com>
2021-03-12 12:27:10 -0500

    pog is dead :crab:

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ with open("tokenfile", "r") as tokenfile:
 async def ping(ctx):
 	await ctx.send(f'Pong! {round(client.latency*1000)} ms')
 
-
-
-
 @client.event
 async def on_member_join(member):
 	welcome = discord.utils.get(member.guild.channels, id=766848918499360809)
@@ -30,5 +27,18 @@ async def on_member_remove(member):
         welcome = discord.utils.get(member.guild.channels, id=766848918499360809)
 
         await welcome.send(f"{member.mention} has left the server\n\nwe now have {len(member.guild.members)} members")
+
+pog = ["pog","poj","poog","pooog","pooj","poooj", "po𝑔"]
+
+@client.event
+async def on_message(message):
+	for x in pog:
+		if x in message.content:
+			try:
+				await message.guild.kick(message.author, reason="said p*g")
+				print(f"{message.author} said p*g")
+			except discord.errors.Forbidden:
+				print(f"{message.author} said p*g but i cant kick them")
+			break
 
 client.run(token)
