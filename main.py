b1c6a8c76721e9c697f094c5ae1efdf8a76f1235
gamerer <starkid2u@outlook.com>
2021-09-16 15:54:43 -0400

    complete slash command migration

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import discord
 from discord.ext import commands
+from discord_slash import SlashCommand, SlashContext
+from discord_slash.utils.manage_components import create_button, create_actionrow, wait_for_component, ComponentContext, create_select_option, create_select
+from discord_slash.utils.manage_commands import create_permission
+from discord_slash.model import ButtonStyle, SlashCommandPermissionType
+
 import json
 import random
 import sys
@@ -7,6 +12,7 @@ import traceback
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='g!', intents=intents)
+slash = SlashCommand(client, sync_commands=True,debug_guild=766848554899079218)
 
 @client.event
 async def on_ready():
@@ -19,16 +25,12 @@ with open("tokenfile", "r") as tokenfile:
 
 # VVVVVV commands VVVVVV'
 
-@client.command(aliases=['mc'])
+@slash.slash()
 async def membercount(ctx):
-	await ctx.send(f"{len(ctx.guild.members)} members")
+	await ctx.send(f"{len(ctx.guild.members)} members", hidden=hidden)
 
-@client.command()
-async def owo(ctx, *owo):
-	owo = ' '.join(owo)
-	if owo == '':
-		await ctx.send('you need to give me something to owoify')
-		return
+@slash.slash()
+async def owo(ctx, owo, hidden:bool = False):
 	owo = owo.replace('rr','🇷')
 	owo = owo.replace('RR','🇷 🇷')
 	owo = owo.replace('r','w')
@@ -39,143 +41,92 @@ async def owo(ctx, *owo):
 	owo = owo.replace('LL','W')
 	if random.randrange(0, 100) in range(50):
 		owo += '~'
-	await ctx.send(owo)
+	await ctx.send(owo, hidden=hidden)
 
-@client.command()
-async def purge(ctx):
-	if not await client.is_owner(ctx.author):
-		embed = discord.Embed(title="you dont have permission to do this command", colour=discord.Colour.red(), description="become the owner of the bot nerd")
-		await ctx.send(embed=embed)
-		return
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(312292633978339329, SlashCommandPermissionType.USER, True)]})
+async def purge(ctx, messageid, hidden:bool = False):
+	messageid = int(messageid)
 
+	purge = None
 	async for x in ctx.channel.history():
 		try:
-			if x.id == ctx.message.reference.message_id:
+			if x.id == messageid:
 				purge = x
 		except AttributeError:
-			embed = discord.Embed(title="you need to reply to a message", colour=discord.Colour.red())
-			await ctx.send(embed=embed)
+			embed = discord.Embed(title="you need to give a message id", colour=discord.Colour.red())
+			await ctx.send(embed=embed, hidden=hidden)
 			return
+	print(purge.content)
+	
+	async for x in ctx.channel.history(limit=1):
+		message = x
+	print(message.content)
 
-	amount = 1
-	async for x in ctx.channel.history(after=purge, before=ctx.message):
+	amount = 2
+	async for x in ctx.channel.history(after=purge, before=message):
 		amount += 1
 
-	await ctx.channel.purge(after=purge, before=ctx.message)
+	await ctx.channel.purge(after=purge, before=message)
+	await message.delete()
 	await purge.delete()
 	embed = discord.Embed(title=f"{amount} messages purged", colour=discord.Colour.green())
-	await ctx.send(embed=embed)
+	await ctx.send(embed=embed, hidden=hidden)
 
-@client.command()
-async def ping(ctx):
-	await ctx.send(f'Pong! {round(client.latency*1000)} ms')
+@slash.slash()
+async def ping(ctx, hidden:bool = False):
+	await ctx.send(f'Pong! {round(client.latency*1000)} ms', hidden=hidden)
 
-@client.command()
-async def wii(ctx, *game):
-	game = " ".join(game)
-	gamererr = client.get_user(312292633978339329)
-	await gamererr.send(f'wii game from **{ctx.author}** ```{game}```')
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(766849548277645313, SlashCommandPermissionType.ROLE, True)]})
+async def wii(ctx, game:str, hidden:bool = False):
+	rhozeta = client.get_user(312292633978339329)
+	await rhozeta.send(f'wii game from **{ctx.author}** ```{game}```')
 	print(f'wii game from {ctx.author}, {game}')
-	await ctx.send(f'suggestion taken ```{game}```')
+	await ctx.send(f'suggestion taken ```{game}```', hidden=hidden)
 
-@client.command()
-async def kick(ctx, user, *reason):
-
-	reason = " ".join(reason)
-
-	if reason == "":
-		reason = "no reason given"
-
-	if ctx.message.mentions != []:
-		user = ctx.message.mentions
-	else:
-		user = client.get_user(int(user))
-		if not user:
-			await("you need to ping someone or give a user id")
-			return
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(766849548277645313, SlashCommandPermissionType.ROLE, True)]})
+async def kick(ctx, user:discord.Member, reason:str, hidden:bool = False):
 
 	if ctx.channel.permissions_for(ctx.author).kick_members:
 		try:
-			await user.send(f"kicked by {ctx.message.author} for `{reason}`")
-			await ctx.guild.kick(user, reason=f"kicked by {ctx.message.author} for {reason}")
-			await ctx.send(f"kicked {user.name} for `{reason}`")
+			await user.send(f"kicked by {ctx.author} for `{reason}`")
+			await ctx.guild.kick(user, reason=f"kicked by {ctx.author} for {reason}")
+			await ctx.send(f"kicked {user.name} for `{reason}`", hidden=hidden)
 		except discord.errors.HTTPException:
-			await ctx.guild.kick(user, reason=f"kicked by {ctx.message.author} for {reason}")
-			await ctx.send(f"kicked {user.name} for `{reason}`")
+			await ctx.guild.kick(user, reason=f"kicked by {ctx.author} for {reason}")
+			await ctx.send(f"kicked {user.name} for `{reason}`", hidden=hidden)
 	else:
 		embed = discord.Embed(title="you dont have permission to do this command", colour=discord.Colour.red(), description="become a mod nerd")
 
-		await ctx.send(embed=embed)
+		await ctx.send(embed=embed, hidden=hidden)
 
-@client.command()
-async def ban(ctx, user, *reason):
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(766849548277645313, SlashCommandPermissionType.ROLE, True)]})
+async def ban(ctx, user:discord.Member, reason:str, hidden:bool = False):
+	try:
+		await user.send(f"banned by {ctx.author} for `{reason}`")
+		await ctx.guild.ban(user, reason=f"banned by {ctx.author} for {reason}", delete_message_days=0)
+		await ctx.send(f"banned {user.name} for `{reason}`", hidden=hidden)
+	except discord.errors.HTTPException:
+		await ctx.guild.ban(user, reason=f"banned by {ctx.author} for {reason}", delete_message_days=0)
+		await ctx.send(f"banned {user.name} for `{reason}`", hidden=hidden)
 
-	if reason == []:
-		reason = "no reason"
-	else:
-		reason = " ".join(reason)
-
-	if ctx.message.mentions != []:
-		user = ctx.message.mentions
-	else:
-		user = client.get_user(int(user))
-		if not user:
-			await("you need to ping someone or give a user id")
-			return
-
-	if ctx.channel.permissions_for(ctx.author).ban_members:
-		try:
-			await user.send(f"banned by {ctx.message.author} for `{reason}`")
-			await ctx.guild.ban(user, reason=f"banned by {ctx.message.author} for {reason}", delete_message_days=0)
-			await ctx.send(f"banned {user.name} for `{reason}`")
-		except discord.errors.HTTPException:
-			await ctx.guild.ban(user, reason=f"banned by {ctx.message.author} for {reason}", delete_message_days=0)
-			await ctx.send(f"banned {user.name} for `{reason}`")
-	else:
-		embed = discord.Embed(title="you dont have permission to do this command", colour=discord.Colour.red(), description="become a mod nerd")
-
-		await ctx.send(embed=embed)
-
-@client.command()
-async def mute(ctx, user, *reason):
-
-	if reason == []:
-		reason = "no reason"
-	else:
-		reason = " ".join(reason)
-
-	if ctx.message.mentions != []:
-		user = ctx.message.mentions
-	else:
-		user = client.get_user(int(user))
-		if not user:
-			await("you need to ping someone or give a user id")
-			return
-
-	muted = client.get_role(774294917299830824)
-
-	await user.send(f"you were **muted** for {reason}")
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(766849548277645313, SlashCommandPermissionType.ROLE, True)]})
+async def mute(ctx, user:discord.Member, reason:str, hidden:bool = False):
+	muted = ctx.guild.get_role(774294917299830824)
+	try:
+		await user.send(f"you were **muted** for {reason}")
+		await ctx.send(f"muted {user.display_name} for {reason}", hidden=hidden)
+	except discord.errors.Forbidden:
+		await ctx.send(f"muted {user.display_name} for {reason} but i couldnt message them", hidden=hidden)
 	await user.add_roles(muted)
 
-@client.command()
-async def unmute(ctx, user, *reason):
-
-	if reason == []:
-		reason = "no reason"
-	else:
-		reason = " ".join(reason)
-
-	if ctx.message.mentions != []:
-		user = ctx.message.mentions
-	else:
-		user = client.get_user(int(user))
-		if not user:
-			await("you need to ping someone or give a user id")
-			return
-
-	muted = client.get_role(774294917299830824)
-
-	await user.send(f"you were **unmuted** for {reason}")
+@slash.slash(default_permission=False, permissions={766848554899079218: [create_permission(766849548277645313, SlashCommandPermissionType.ROLE, True)]})
+async def unmute(ctx, user:discord.Member, reason:str, hidden:bool = False):
+	muted = ctx.guild.get_role(774294917299830824)
+	try:
+		await user.send(f"you were **unmuted** for {reason}")
+		await ctx.send(f"unmuted `{user.display_name}` for {reason}", hidden=hidden)
+	except discord.errors.Forbidden:
+		await ctx.send(f"unmuted `{user.display_name}` for `{reason}` but i couldnt message them", hidden=hidden)
 	await user.remove_roles(muted)
 
 # VVVVVV events VVVVVV
@@ -192,21 +143,5 @@ async def on_member_remove(member):
 	welcome = client.get_channel(766848918499360809)
 
 	await welcome.send(f"{member.mention} has left the server\n\nwe now have {len(member.guild.members)} members")
-	await member.send(f"thanks for joining **{member.guild.name}**, we hope to see you again")
-
-@client.event
-async def on_command_error(ctx:commands.Context, exception):
-	embed = discord.Embed(color=discord.Color.red())
-	if type(exception) is commands.errors.MissingRequiredArgument:
-		embed.title = "You forgot an argument"
-		embed.description = f"The syntax to `{client.command_prefix}{ctx.command.name}` is `{client.command_prefix}{ctx.command.name} {ctx.command.signature}`."
-		await ctx.send(embed=embed)
-	elif type(exception) is commands.CommandNotFound:
-		embed.title = "Invalid command"
-		embed.description = f"The command you just tried to use is invalid. Use `{client.command_prefix}help` to see all commands."
-		await ctx.send(embed=embed)
-	else:
-		print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
-		traceback.print_exception(type(exception), exception, exception.__traceback__, file=sys.stderr)
 
 client.run(token)
